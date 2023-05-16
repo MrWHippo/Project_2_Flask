ee10069f4220b6c87300f87a9707f7491baaf328
Jonty Radcliffe <17radcj@olab.org.uk>
2023-05-16 16:01:13 +0100

    finished initial

--- spysite/app.py
+++ spysite/app.py
@@ -6,6 +6,8 @@ from wtforms import StringField, SubmitField
 app = Flask(__name__)
 app.config['SECRET_KEY']='tb3njt39h8fi20'
 
+login_history =[]
+
 class LoginForm(FlaskForm):
     username = StringField("Username")
     submit = SubmitField("Log In")
@@ -13,6 +15,18 @@ class LoginForm(FlaskForm):
 @app.route("/", methods=["GET","POST"])
 def login():
     entry_form= LoginForm()
-    return render_template("login.html", form=entry_form)
+
+    if entry_form.is_submitted():
+        username = entry_form.username.data
+        if username == "":
+            return render_template("login.html", form=entry_form)
+        elif username != "admin":
+            login_history.append((username,datetime.now().strftime("%H:%M:%S")))
+            return render_template("user.html", username=username, count=len(login_history))
+        else:
+            return render_template("admin.html", history=login_history)
+    else:
+        print("Form was not submitted.")
+        return render_template("login.html", form=entry_form)
 
 
